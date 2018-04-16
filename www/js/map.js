891a55027dd1e11d1bc1c72d2d55c3e4866b50c0
ePareto Dev <servicedesk@epareto.com>
2018-04-16 10:30:39 +0000

    revised map for nearest police station to add detailed address (WIP)

--- www/js/map.js
+++ www/js/map.js
@@ -1,49 +1,49 @@
 var map, infoWindow;
-    var initMap = function () {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: -34.397, lng: 150.644 },
-            zoom: 8
+var initMap = function () {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 5.5913754, lng: -0.2497701 },
+        zoom: 8
+    });
+    var kmlLayer = new google.maps.KmlLayer("https://maps.google.ca/maps/ms?ie=UTF8&msa=0&output=kml&msid=210000746142049190989.0004ce14b7a8e95167602", { map: map, preserveViewport: true });
+    kmlLayer.setMap(map);
+
+
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            //infoWindow.open(map);
+            map.setCenter(pos);
+
+            // places search
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: pos,
+                radius: 500,
+                type: ['police']
+            }, callback);
+
+            //places details
+
+
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
         });
-        var kmlLayer = new google.maps.KmlLayer("https://maps.google.ca/maps/ms?ie=UTF8&msa=0&output=kml&msid=210000746142049190989.0004ce14b7a8e95167602", { map: map, preserveViewport: true });      
-        kmlLayer.setMap(map);
-
-
-        infoWindow = new google.maps.InfoWindow;
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('Location found.');
-                infoWindow.open(map);
-                map.setCenter(pos);
-
-                // places search
-                var service = new google.maps.places.PlacesService(map);
-                service.nearbySearch({
-                    location: pos,
-                    radius: 500,
-                    type: ['police']
-                }, callback);
-
-                //places details
-                
-
-            }, function () {
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
     }
-    console.log('Got here');
-    google.maps.event.addDomListener(window, 'load', initMap());
+}
+console.log('Got here');
+google.maps.event.addDomListener(window, 'load', initMap());
 
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -63,6 +63,7 @@ function callback(results, status) {
 }
 
 function createMarker(place) {
+    var infowindow = new google.maps.InfoWindow;
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
         map: map,
