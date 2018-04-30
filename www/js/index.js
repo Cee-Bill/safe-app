fdcc5683515d2c24a682b89fe07f99f89112aeaf
jonathan <jonathan@LAPTOP-ITG1TLVT>
2018-04-30 16:33:10 +0000

    added inapp browser

--- www/js/index.js
+++ www/js/index.js
@@ -623,16 +623,18 @@ myApp.onPageInit('dashboard', function(page) {
 $$('#powersave').on('click',function(){
     myApp.closeModal();
     powersaveToggle();
-    myApp.alert("GPS powersaving mode enabled, the app will use location only when requested");
+    
 })
 
 function powersaveToggle(){
     if(window.powersave == true){
         window.powersave == false;
+        myApp.alert("GPS powersaving mode enabled, the app will use location only when requested");
     }
     
     else{
-        widow.powersave = true;
+        window.powersave = true;
+        myApp.alert("GPS powersaving mode disabled, the app will continuously use location services");
     }
 }
     
